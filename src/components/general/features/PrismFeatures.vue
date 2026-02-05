e1728c80aaf6a9d93de7000d747c3a7bc4f10be1
notwindstone <notwindstone@gmail.com>
2026-02-05 20:41:09 +0300

    fix: update the prismlauncher background svg address

--- src/components/general/features/PrismFeatures.vue
+++ src/components/general/features/PrismFeatures.vue
@@ -6,7 +6,7 @@ import Image from "@/components/base/Image.vue";
   <div class="relative w-full flex flex-1 overflow-hidden rounded-md px-4 lg:w-auto">
     <Image
       class-names="absolute left-0 right-0 top-0 aspect-[3/1] w-full rounded-md object-cover"
-      src="https://prismlauncher.org/img/background/prism-background.svg"
+      src="https://prismlauncher.org/img/background/prism-backgroundDark.svg"
       alt="Prism Launcher's banner"
     />
     <div class="aspect-[3/1] w-full" />
