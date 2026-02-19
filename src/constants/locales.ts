19d616fc9484f2fe4bf54272ef44fdbb809001ed
MiguVT <cuentas@miguvt.com>
2026-02-20 00:12:06 +0100

    feat: add Spanish locale support in loacles constants and organize the order of locales in the list based on the most spoken languages in the world

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,6 +1,10 @@
 import type { LocaleType } from "@/types/locale.type.ts";
 
-export const Locales = ["en", "ru", "ua", "fa", "pl"] as const;
+/*
+ * This file contains the list of supported locales and their corresponding information.
+ * They are ordered based on the number of speakers worldwide, remember to follow this order when adding new locales.
+ */
+export const Locales = ["en", "es", "ru", "pl", "fa", "ua"] as const;
 export const ExtraInfo: Record<LocaleType, {
   "Name": string;
   "Flag": string;
@@ -9,20 +13,24 @@ export const ExtraInfo: Record<LocaleType, {
     "Name": "English",
     "Flag": "\uD83C\uDDEC\uD83C\uDDE7",
   },
+  "es": {
+    "Name": "Español",
+    "Flag": "\uD83C\uDDEA\uD83C\uDDF8",
+  },
   "ru": {
     "Name": "Русский",
     "Flag": "\uD83C\uDDF7\uD83C\uDDFA",
   },
-  "ua": {
-    "Name": "Українська",
-    "Flag": "\uD83C\uDDFA\uD83C\uDDE6",
+  "pl": {
+    "Name": "Polski",
+    "Flag": "\uD83C\uDDF5\uD83C\uDDF1",
   },
   "fa": {
     "Name": "فارسی",
     "Flag": "\uD83C\uDDEE\uD83C\uDDF7",
   },
-  "pl": {
-    "Name": "Polski",
-    "Flag": "\uD83C\uDDF5\uD83C\uDDF1",
+  "ua": {
+    "Name": "Українська",
+    "Flag": "\uD83C\uDDFA\uD83C\uDDE6",
   },
 };
